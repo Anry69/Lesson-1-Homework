274c4ea826569cf51bc4b8f68c95167f880a1e11
Alexey <ccrowen@gmail.com>
2022-02-10 22:10:32 +0300

    Task 2-2

--- Homework/Task_2/Program.cs
+++ Homework/Task_2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти максимальное из трёх чисел.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СиШарп
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            double c = double.Parse(Console.ReadLine());
+            if (a < b) a = b;
+            if (a < c) a = c;
+            Console.WriteLine("Max = {0}",a);
+            Console.ReadKey();
+        }
+    }
+}

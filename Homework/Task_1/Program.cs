83b09b866de9f35e5e62b8c16e93b88e6092ab1c
Alexey <ccrowen@gmail.com>
2022-02-10 21:43:23 +0300

    Первое задание

---
+++ Homework/Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Даны два числа. Показать большее и меньшее число.
+
+using System;
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 3, b = 8;
+        try
+        {
+            throw new Exception(a > b ? a + " " + b : b + " " + a);
+        }
+        catch(Exception exc)
+        {
+            Console.WriteLine(exc.Message);
+        }
+    }
+}

1ecb2db39bf4cda268a8343a6322f3c0ea985445
Alexey <ccrowen@gmail.com>
2022-02-10 22:26:48 +0300

    Task 4

---
+++ Homework/Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать чётные числа от 1 до N.
+
+using System;
+ 
+class Program
+{
+    public static void Main()
+    {
+        Action<int>[] f = new Action<int>[] { n => Console.Write(n), n => Console.Write(' ') };
+        for (int i = 2; i <= 50; i++)
+        {
+            f[i & 1](i);
+        }
+    }
+}
